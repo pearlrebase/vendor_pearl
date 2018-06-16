4d3cdde2f5b5a03ad13abde9df60a36cd5ee4db5
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-17 03:55:59 +0530

    potato: Set default overlays (1/3)
    - Use Amber as default accent and Stocktato as default base
    
    Change-Id: I251e9e94cf96e54a629f60c7abd793e566e0f50e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -27,7 +27,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
-    ro.build.selinux=1
+    ro.build.selinux=1 \
+    ro.boot.vendor.overlay.theme=com.potato.overlay.accent.amber;com.potato.overlay.base.stockfixed
 
 # LatinIME gesture typing
 ifneq ($(filter tenderloin,$(TARGET_PRODUCT)),)
