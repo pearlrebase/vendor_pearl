a49b49b6cff45c44eafdb3003770b88598593018
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 14:23:57 -0700

    vendor/potato: Add support for java source overlays
    *) Set JAVA_SOURCE_OVERLAYS in a device makefile as follows:
       # Java sourcefile overlays (for Android.bp built modules only)
       # Format is a whitespace separated set of rules, each of which
       # structured as follows:
       # modulename|overlay directory|glob pattern within overlay dir
       JAVA_SOURCE_OVERLAYS := \
               org.potato.hardware|$(LOCAL_PATH)/potatohw|**/*.java
    
    *) As of this commit, java sources overlays are only allowed for
       module org.potato.hardware.
    
    Change-Id: I6be1c12567081357f5231a84df98ac002c0563b4
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ build/soong/android/config.go
@@ -0,0 +1,13 @@
+package android
+
+// Global config used by Pearl soong additions
+var PearlConfig = struct {
+	// List of packages that are permitted
+	// for java source overlays.
+	JavaSourceOverlayModuleWhitelist []string
+}{
+	// JavaSourceOverlayModuleWhitelist
+	[]string{
+		"org.pearl.hardware",
+	},
+}
