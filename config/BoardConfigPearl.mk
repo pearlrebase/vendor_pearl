5b26713f6204c43e040deee0e0445a477d54d098
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-21 18:22:35 -0700

    Automatically set soong namespace when setting project pathmap
    Change-Id: I5629cd4763389b4767481942df666da6cb8741ed

--- config/BoardConfigPearl.mk
+++ config/BoardConfigPearl.mk
@@ -4,3 +4,5 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/pearl/config/BoardConfigQcom.mk
 endif
 include vendor/pearl/config/BoardConfigSoong.mk
+
+PRODUCT_SOONG_NAMESPACES += $(PATHMAP_SOONG_NAMESPACES)
