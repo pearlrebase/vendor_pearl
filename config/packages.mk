9f6edc283172e0d221dc8a536dee58135cf90a8e
Shreyansh Lodha <slodha96@gmail.com>
2018-10-07 22:11:02 +0530

    Add Google Sans and define it's usage
    Change-Id: I204661a3b0378f73d2139f0ac4a94743a3823d24
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -45,3 +45,13 @@ PRODUCT_PACKAGES += \
     SystemSettingsIconTint \
 	SysuiQsBlackTheme \
     SysuiQsDarkTheme
+
+PRODUCT_COPY_FILES += \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+   vendor/pearl/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/pearl/prebuilt/common/fonts/google-sans.xml
