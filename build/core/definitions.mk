f0d7d2b6c15af8d6306259dbfb955df063af4a3d
Ethan Chen <intervigil@gmail.com>
2019-06-22 13:30:38 -0700

    build: Reorganize inclusion of QCOM macros
    * There's actually not a whole lot QC-specific about these macros,
      make them available at definition.mk inclusion time rather than
      relying on BoardConfig to include them.
    * Don't create a dependency on QCOM macros from a makefile
      that is included very early in the build process.
    
    Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -3,4 +3,4 @@ $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
 
 # Include QCOM macros
-include vendor/superior/build/core/qcom_utils.mk
+include vendor/pearl/build/core/qcom_utils.mk
