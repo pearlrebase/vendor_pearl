Cardinal-AOSP/vendor_cardinal

[2/2] vendor: add OTA script do delete package cache
after flashing new image delete contents of /data/system/package_cache/
to make sure they are recreated. Outdated cache files are the
reason for the famous 'resource derps'

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

common.mk
@@ -65,6 +65,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cardinal/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/cardinal/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # Changelog
 ifeq ($(CARDINAL_RELEASE),true)
 PRODUCT_COPY_FILES +=  \

pearlrebase/vendor_pearl

Change-Id: Ia50b8223c07d6eeb09c404599138827be911063b

config/common.mk
@@ -46,6 +46,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/pearl/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/potato/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/pearl/overlay/common
 