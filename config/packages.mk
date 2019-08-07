PotatoProject/vendor_potato

Add turbo with permission and sysconfig
Change-Id: Ia75be3b8dea17ae4e42bad159d96145e6617258a
Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

config/packages.mk
@@ -48,3 +48,9 @@ PRODUCT_COPY_FILES += \
    vendor/potato/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
 
 ADDITIONAL_FONTS_FILE := vendor/potato/prebuilt/common/fonts/google-sans.xml
+
+# Turbo
+PRODUCT_PACKAGES += \
+    Turbo \
+	vendor/potato/prebuilt/common/etc/permissions/privapp-permissions-turbo.xml:system/etc/permissions/privapp-permissions-turbo.xml \
+	vendor/potato/prebuilt/common/etc/sysconfig/turbo.xml:system/etc/sysconfig/turbo.xml

pearlrebase/vendor_pearl

config/packages.mk
@@ -55,3 +55,11 @@ PRODUCT_COPY_FILES += \
    vendor/pearl/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
 
 ADDITIONAL_FONTS_FILE := vendor/pearl/prebuilt/common/fonts/google-sans.xml
+
+# Turbo
+PRODUCT_PACKAGES += \
+    Turbo \
+
+PRODUCT_COPY_FILES += \
+   vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-turbo.xml:system/etc/permissions/privapp-permissions-turbo.xml \
+   vendor/pearl/prebuilt/common/etc/sysconfig/turbo.xml:system/etc/sysconfig/turbo.xml