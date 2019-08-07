2aa4ffb15cf702963745c71cd3a7fe29e2bd1afd
aviraxp <aviraxp@users.noreply.github.com>
2019-08-07 05:34:40 +0000

    config: Add Deskclock to power whitelist
     * Seems sometimes alarm cannot ring because of doze.
     * Also add GoogleDeskClock - @jhenrique09

--- config/common.mk
+++ config/common.mk
@@ -74,6 +74,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=30 \
     ro.config.bt_sco_vol_steps=30
 
+# Power whitelist
+PRODUCT_COPY_FILES += \
+    vendor/pearl/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
+
 # Disable Rescue Party
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true
