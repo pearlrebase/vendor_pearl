SuperiorOS/android_vendor_superior

vendor: Fruity pebbles [2/4] *Dark theme edits* - Add in styles to theme the package installer - Theme DND QS panel text - Theme Recents FAB - Theme conditions/suggestions background
Change-Id: Ie6e916691449af4b4c3d652d2be6fd9924ac3c8b

config/common.mk
@@ -173,6 +173,9 @@ $(call inherit-product, vendor/superior/config/telephony.mk)
 # Themes
 include vendor/themes/common.mk
 
+# Packages
+include vendor/superior/config/packages.mk
+
 # Superior_props
 $(call inherit-product, vendor/superior/config/superior_props.mk)
 
@@ -187,4 +190,3 @@ endif
 #        include vendor/superior/sdclang/sdclang.mk
 #    endif
 #endif
-

pearlrebase/vendor_pearl

config/common.mk
@@ -149,24 +149,27 @@ PRODUCT_PACKAGES += \
 
 # Clean cache
 PRODUCT_COPY_FILES += \
-    vendor/superior/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
-	
+    vendor/pearl/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # Recommend using the non debug dexpreopter
 USE_DEX2OAT_DEBUG ?= false
 
 #Telephony
 $(call inherit-product, vendor/pearl/config/telephony.mk)
 
-# pearl_props
+# Pearl_props
 $(call inherit-product, vendor/pearl/config/pearl_props.mk)
+
+# Packages
+include vendor/pearl/config/packages.mk
+
+#Themes
+include vendor/pearl/themes/common.mk
 
 # Enable ADB authentication
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 endif
-
-#Themes
-include vendor/pearl/themes/common.mk
 
 # Include SDCLANG definitions if it is requested and available
 #ifeq ($(HOST_OS),linux)