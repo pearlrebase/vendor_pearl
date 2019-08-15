SuperiorOS/android_vendor_superior

Superior: Switch to Lawnchair
- CI build Lawnchair-alpha_571
- Thanks @deletescape :)

Change-Id: Ia31a36cf899a5244536a24ca963661317b2f9146
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/common.mk
@@ -118,6 +118,11 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_PROPERTY_OVERRIDES += \
     org.pixelexperience.weather.revision=2
 
+# Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/potato/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/potato/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 # Bootanimation
 include vendor/superior/config/bootanimation.mk
 

pearlrebase/vendor_pearl

config/common.mk
@@ -117,6 +117,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/pearl/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 #Telephony
 $(call inherit-product, vendor/pearl/config/telephony.mk)
 