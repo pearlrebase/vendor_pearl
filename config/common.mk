SuperiorOS/android_vendor_superior

vendor: Whitelist LiveDisplay permissions
Change-Id: Iefa5365b6ec013f164a338038b0d13c45b369a13

config/common.mk
@@ -155,6 +155,10 @@ include vendor/superior/config/bootanimation.mk
 #Telephony
 $(call inherit-product, vendor/superior/config/telephony.mk)
 
+# LiveDisplay
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml
+
 # Some Apps
 PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/common/priv-app/MatchmakerPrebuilt.apk:system/priv-app/MatchmakerPrebuilt/MatchmakerPrebuilt.apk \

pearlrebase/vendor_pearl

config/common.mk
@@ -132,6 +132,10 @@ $(call inherit-product, vendor/pearl/config/telephony.mk)
 # Pearl_props
 $(call inherit-product, vendor/pearl/config/pearl_props.mk)
 
+# LiveDisplay
+PRODUCT_COPY_FILES += \
+    vendor/pearl/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml
+
 # Packages
 include vendor/pearl/config/packages.mk
 