LineageOS/android_vendor_lineage

build: Separate platform macros from QCOM platform definition
* The macros used in the file formerly called "qcom_utils.mk" are
  actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
  to a QCOM-specific location, and name the remaning macros something more
  generic.

Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c

build/core/definitions.mk
@@ -5,5 +5,8 @@ define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
 
-# Include QCOM macros
-include vendor/lineage/build/core/qcom_utils.mk
+# Include board/platform macros
+include vendor/lineage/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/lineage/build/core/vendor/*.mk

pearlrebase/vendor_pearl

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

build/core/definitions.mk
@@ -2,5 +2,8 @@ define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
 
-# Include QCOM macros
-include vendor/pearl/build/core/qcom_utils.mk
+# Include board/platform macros
+include vendor/pearl/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/pearl/build/core/vendor/*.mk